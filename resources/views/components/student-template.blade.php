2e8dbd6777556da6495537d5217763899b9b0f7f
railee091 <74151748+railee091@users.noreply.github.com>
2021-03-24 19:50:45 +0800

    side navigation and content area for student index
    navigation

--- resources/views/components/student-template.blade.php
+++ resources/views/components/student-template.blade.php
@@ -31,7 +31,7 @@
 	        </ul>
 	        <ul class="navbar-nav">
 	            <li class="nav-item">
-	                <a class="nav-link" href="/LogoutUser" title="logout">name<i class="fa fa-sign-out"></i></a>
+	                <img class="circleLogo" src="{{ asset('img/placeholder-male-square.png') }}" width=30rem">user</img>
 	            </li>
 	            <li class="nav-item">
 	                <a class="nav-link" href="#" data-toggle="modal" data-target="#signUpModal" title="setting"><i class="fa fa-bell"></i></a>
@@ -47,5 +47,97 @@
 	    	{{ $err }}
 	    @endforeach
 	@endif
+	
+	<nav class="main-menu ease-loadLeft">
+	    <ul>
+	        <li>
+	            <a href="">
+	                <i class="fa fa-home fa-2x"></i>
+	                <span class="nav-text">
+	                    Dashboard
+	                </span>
+	            </a>
+	          
+	        </li>
+	        <li class="has-subnav">
+	            <a href="#">
+	                <i class="fa fa-laptop fa-2x"></i>
+	                <span class="nav-text">
+	                    Stars Components
+	                </span>
+	            </a>
+	            
+	        </li>
+	        <li class="has-subnav">
+	            <a href="#">
+	               <i class="fa fa-list fa-2x"></i>
+	                <span class="nav-text">
+	                    Forms
+	                </span>
+	            </a>
+	            
+	        </li>
+	        <li class="has-subnav">
+	            <a href="#">
+	               <i class="fa fa-folder-open fa-2x"></i>
+	                <span class="nav-text">
+	                    Pages
+	                </span>
+	            </a>
+	           
+	        </li>
+	        <li>
+	            <a href="#">
+	                <i class="fa fa-bar-chart-o fa-2x"></i>
+	                <span class="nav-text">
+	                    Graphs and Statistics
+	                </span>
+	            </a>
+	        </li>
+	        <li>
+	            <a href="#">
+	                <i class="fa fa-font fa-2x"></i>
+	                <span class="nav-text">
+	                   Quotes
+	                </span>
+	            </a>
+	        </li>
+	        <li>
+	           <a href="#">
+	               <i class="fa fa-table fa-2x"></i>
+	                <span class="nav-text">
+	                    Tables
+	                </span>
+	            </a>
+	        </li>
+	        <li>
+	           <a href="#">
+	                <i class="fa fa-map-marker fa-2x"></i>
+	                <span class="nav-text">
+	                    Maps
+	                </span>
+	            </a>
+	        </li>
+	        <li>
+	            <a href="#">
+	               <i class="fa fa-info fa-2x"></i>
+	                <span class="nav-text">
+	                    Documentation
+	                </span>
+	            </a>
+	        </li>
+	    </ul>
+
+	    <ul class="logout">
+	        <li>
+	           <a href="#">
+	                 <i class="fa fa-power-off fa-2x"></i>
+	                <span class="nav-text">
+	                    Logout
+	                </span>
+	            </a>
+	        </li>  
+	    </ul>
+	</nav>
 </body>
 </html>
--- resources/views/student/studentindex.blade.php
+++ resources/views/student/studentindex.blade.php
@@ -1,8 +1,19 @@
 <x-student-template data="studentindex" />
-<div class="container">
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<h1>STUDENT INDEX</h1>
+<div class="area"> <!-- put all content inside area, outside area will mess with side naviagtion-->
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm">
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm">
+				<h1>STUDENT INDEX</h1>
+			</div>
+		</div>
+	</div>
 </div>
