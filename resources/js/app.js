081f34e34c2f712917927c0288476770df2e39a6
railee091 <74151748+railee091@users.noreply.github.com>
2021-03-24 18:08:40 +0800

    reset files to "student index" commit
    removes react.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,1 @@
 require('./bootstrap');
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-/**
- * Next, we will create a fresh React component instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-require('./components/Example');
-@import "node_modules/font-awesome/scss/font-awesome.scss";
-=======
->>>>>>> parent of 2cd3813 (react js installation)
-=======
->>>>>>> parent of 2cd3813 (react js installation)
