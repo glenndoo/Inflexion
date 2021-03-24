ffdc8808cf158a87fc02b3abc07c351838ad6eae
railee091 <74151748+railee091@users.noreply.github.com>
2021-03-24 14:20:05 +0800

    studentindex.blade tests
    test

--- resources/views/student/studentindex.blade.php
+++ resources/views/student/studentindex.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student</title>
-</head>
-<body>
-    Student index 
-</body>
-</html>
+<x-template data="Restricted" />
+<div class="container">
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	<h1>STUDENT INDEX</h1>
+</div>
