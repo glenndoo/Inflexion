28282403da8723f39f25f69e130028dd76e3eab1
railee091 <74151748+railee091@users.noreply.github.com>
2021-03-27 14:48:39 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -81,16 +81,16 @@ Route::group(['middleware' => 'Student'], function(){
         'as' => 'studentIndex',
         'uses' => 'StudentController@studentIndex'
     ));
-    Route::get('/studentSettings', function () { //added for testing maiko
+    Route::get('/student/settings', function () { //added for testing maiko
         return view('student/studentsettings');
     });
-    Route::get('/studentProfile', function () { //added for testing maiko
+    Route::get('/student/grofile', function () { //added for testing maiko
         return view('student/studentProfile');
     });
-    Route::get('/studentGroups', function () { //added for testing maiko
+    Route::get('/student/groups', function () { //added for testing maiko
         return view('student/studentgroups');
     });
-    Route::get('/studentinbox', function () { //added for testing maiko
+    Route::get('/student/inbox', function () { //added for testing maiko
         return view('student/studentinbox');
     });
 });
