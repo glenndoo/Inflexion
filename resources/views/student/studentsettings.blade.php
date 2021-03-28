21c3f8a698072add996ee85b277b23eb23ddda0d
railee091 <74151748+railee091@users.noreply.github.com>
2021-03-28 12:37:30 +0800

    Update studentsettings.blade.php

--- resources/views/student/studentsettings.blade.php
+++ resources/views/student/studentsettings.blade.php
@@ -27,21 +27,21 @@
 		                </div>
 		                <!-- start Form Settings -->
 		                <div class="form-content">
-		                	<form>
+		                	<form  class="needs-validation" action="#" oninput='p3.setCustomValidity(p3.value != p2.value ? "Confirm Password does not match." : "") '>
 			                    <div class="row">
 			                        <div class="col-md-4">
 			                            <div class="form-group">
-			                                <input type="text" class="form-control" placeholder="First Name *" value="{{ session()->get('info.userDetails.inflexion_detail_first') }}" name="first"/>
+			                                <input type="text" class="form-control" placeholder="First Name *" value="{{ session()->get('info.userDetails.inflexion_detail_first') }}" name="first" required/>
 			                            </div>
 			                        </div>
 			                        <div class="col-md-4">
 			                            <div class="form-group">
-			                                <input type="text" class="form-control" placeholder="Middle Name*" value="{{ session()->get('info.userDetails.inflexion_detail_middle') }}"/>
+			                                <input type="text" class="form-control" placeholder="Middle Name*" value="{{ session()->get('info.userDetails.inflexion_detail_middle') }}" required/>
 			                            </div>
 			                        </div>
 			                        <div class="col-md-4">
 			                            <div class="form-group">
-			                                <input type="text" class="form-control" placeholder="Last Name*" value="{{ session()->get('info.userDetails.inflexion_detail_last') }}"/>
+			                                <input type="text" class="form-control" placeholder="Last Name*" value="{{ session()->get('info.userDetails.inflexion_detail_last') }}" required/>
 			                            </div>
 			                        </div>
 			                    </div>
@@ -58,8 +58,8 @@
 			                        </div>
 			                        <div class="col-md-4">
 			                            <div class="form-group">
-										    <select class="form-control" id="exampleFormControlSelect1">
-										    	<option selected="true" disabled="disabled">select gender</option>    
+										    <select class="form-control" id="exampleFormControlSelect1" required>
+										    	<option>select gender</option>    
 										     	<option>Male</option>
 										      	<option>Female</option>
 										      	<option>Unspecified</option>
@@ -68,53 +68,50 @@
 			                        </div>
 			                        <div class="col-md-4">
 			                            <div class="form-group">
-										    <select class="form-control" id="exampleFormControlSelect1">
-										    	<option selected="true" disabled="disabled">Citizenship</option>    
-										     	<option>//Get from api</option>
-										    </select>
-										  </div>
+										    <input type="text" class="form-control" value="" name="citizenship" placeholder="citizenship" required/>
+										</div>
 			                        </div>
 			                    </div>
 			                    <hr/>
 			                    <div class="row">
-			                        <div class="col-md-4">
+			                        <div class="col-md-5">
 			                            <div class="form-group">
 			                                <input type="text" class="form-control" placeholder="Email Address*" value="{{ session()->get('info.userDetails.inflexion_username') }}" disabled/>
 			                            </div>
 			                        </div>
-			                        <div class="col-md-2">
+			                        <div class="col-md-3">
 			                            <div class="form-group">
-			                                <input type="text" class="form-control" placeholder="VALIDATED" value="{{ strtoupper(session()->get('info.userDetails.inflexion_user_token')) }} Profile" disabled />
+			                                <input type="text" class="form-control" placeholder="VALIDATED" value="{{ strtoupper(session()->get('info.userDetails.inflexion_user_token')) }} PROFILE" disabled />
 			                            </div>
 			                        </div>
-			                        <div class="col-md-6">
+			                        <div class="col-md-4">
 			                            <div class="form-group">
-			                                <input type="text" class="form-control" placeholder="Phone Number*" value="{{ session()->get('info.userDetails.inflexion_detail_contact') }}"/>
+			                                <input type="text" class="form-control" placeholder="Phone Number*" value="{{ session()->get('info.userDetails.inflexion_detail_contact') }}"required/>
 			                            </div>
 			                        </div>
 			                        <div class="col-md-3">
 			                            <div class="form-group">
-			                                <input type="text" class="form-control" placeholder="Country" value="{{ session()->get('info.userDetails.inflexion_detail_country') }}"/>
+			                                <input type="text" class="form-control" placeholder="Country" value="{{ session()->get('info.userDetails.inflexion_detail_country') }}"required/>
 			                            </div>
 			                        </div>
 			                        <div class="col-md-3">
 			                            <div class="form-group">
-			                                <input type="text" class="form-control" placeholder="State / province" value="{{ session()->get('info.userDetails.inflexion_detail_state') }}"/>
+			                                <input type="text" class="form-control" placeholder="State / province" value="{{ session()->get('info.userDetails.inflexion_detail_state') }}"required/>
 			                            </div>
 			                        </div>
 			                        <div class="col-md-2">
 			                            <div class="form-group">
-			                                <input type="text" class="form-control" placeholder="city / town" value="{{ session()->get('info.userDetails.inflexion_detail_city') }}"/>
+			                                <input type="text" class="form-control" placeholder="city / town" value="{{ session()->get('info.userDetails.inflexion_detail_city') }}"required/>
 			                            </div>
 			                        </div>
 			                        <div class="col-md-2">
 			                            <div class="form-group">
-			                                <input type="text" class="form-control" placeholder="Zip Code" value="{{ session()->get('info.userDetails.inflexion_detail_zip') }}"/>
+			                                <input type="text" class="form-control" placeholder="Zip Code" value="{{ session()->get('info.userDetails.inflexion_detail_zip') }}"required/>
 			                            </div>
 			                        </div>
 			                        <div class="col-md-2">
 			                            <div class="form-group">
-			                                <input type="text" class="form-control" placeholder="street" value="{{ session()->get('info.userDetails.inflexion_detail_street') }}"/>
+			                                <input type="text" class="form-control" placeholder="street" value="{{ session()->get('info.userDetails.inflexion_detail_street') }}" required/>
 			                            </div>
 			                        </div>
 			                    </div>
@@ -141,21 +138,21 @@
 			                    <div class="row">
 			                        <div class="col-md-4">
 			                            <div class="form-group">
-			                                <input type="text" class="form-control" placeholder="Current Password" value=""/>
+			                                <input type="text" class="form-control" placeholder="Current Password"name="p1" required/>
 			                            </div>
 			                        </div>
 			                        <div class="col-md-4">
 			                            <div class="form-group">
-			                                <input type="text" class="form-control" placeholder="New Password" value=""/>
+			                                <input type="text" class="form-control" placeholder="New Password" name="p2" required/>
 			                            </div>
 			                        </div>
 			                        <div class="col-md-4">
 			                            <div class="form-group">
-			                                <input type="text" class="form-control" placeholder="Confirm  New Password" value=""/>
+			                                <input type="text" class="form-control" placeholder="Confirm  New Password" name="p3" required/>
 			                            </div>
 			                         </div>
 			                    </div>
-			                    <button type="button" class="btn btn-primary Submit">Submit</button>
+			                    <button type="submit" class="btn btn-primary Submit">Submit</button>
 		                	</form>
 		                </div>
 		                 <!-- End Form Settings -->
@@ -166,3 +163,23 @@
 		<!--end settings-->
 	</div>
 </div>
+<script>
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+</script>
