a813c4755cd6c42a289c6d062611f9d8c966ea67
railee091 <74151748+railee091@users.noreply.github.com>
2021-03-29 16:47:43 +0800

    Update studentclasses.blade.php

--- resources/views/student/studentclasses.blade.php
+++ resources/views/student/studentclasses.blade.php
@@ -56,81 +56,75 @@
 					  		<h6>BOOKED CLASSES</h6>
 
 					  		<div class="col-sm-9">
-						  		<!--start results for groups not joined -->
-								<div class="row ">
-									<div class="col-sm-12">
-										<div class="group-card card shadow">
-									  		<div class="card-header">
-									  			<div class="row">
-									  				<div class="col-sm-1">
-									  					<time datetime="2014-06-29" class="pull-left datebox">
-															<strong>Mar</strong>
-															<span>29</span>
-														</time>
-									  				</div>
-									  				<div class="col-sm-7">
-									  					<h4 title="Your Tutor for this class">
-									  						 Glenn Dumaguing
-									  						<button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#tutorInfo_1">
-									  							<i class="fa fa-user" aria-hidden="true"></i>
-									  						</button>
-									  					</h4>
-									  					<h6>Monday March 29 2021 at 3:30pm</h6>
-									  				</div>
-									  				<div class="col-sm-4 text-right">
-									  					<a href="#" class="btn btn-success form-control">Mark as Completed</a>
-									  				</div>
-									  			</div>
-									  		</div>
-									  		<div class="card-body">
-									    		ENGLISH FOR BEGGINERS
-									  		</div>
+						  		<!--start results for booked -->
+						  		@foreach(range(date('d')-3, date('d')) as $d)
+								    <div class="row ">
+										<div class="col-sm-12">
+											<div class="group-card card shadow">
+										  		<div class="card-header">
+										  			<div class="row">
+										  				<div class="col-sm-1">
+										  					<time datetime="2014-06-29" class="pull-left datebox">
+																<strong>Mar</strong>
+																<span>{{$d}}</span>
+															</time>
+										  				</div>
+										  				<div class="col-sm-7">
+										  					<h4 title="Your Tutor for this class">
+										  						 Glenn Dumaguing
+										  						<button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#tutorInfo_{{$d}}">
+										  							<i class="fa fa-user" aria-hidden="true"></i>
+										  						</button>
+										  					</h4>
+										  					<h6>Monday March {{$d}} 2021 at 3:30pm</h6>
+										  				</div>
+										  				<div class="col-sm-4 text-right">
+										  					<a href="#" class="btn btn-success form-control">Mark as Completed</a>
+										  				</div>
+										  			</div>
+										  		</div>
+										  		<div class="card-body">
+										  			<div class="row">
+										  				<div class="col-sm-8">
+											    		ENGLISH FOR BEGGINERS
+											  			</div>
+											  			<div class="col-sm-2">
+										  					<a href="#" class="btn btn-danger form-control">cancel</a>
+										  				</div>
+											  			<div class="col-sm-2">
+										  					<a href="#" class="btn btn-primary form-control">resched</a>
+										  				</div>
+										  			</div>
+										  			
+										  		</div>
+											</div>
 										</div>
 									</div>
-								</div>
 
-								<hr/><!--break to next entry-->
-
-								<div class="row ">
-									<div class="col-sm-12">
-										<div class="group-card card shadow">
-									  		<div class="card-header">
-									  			<div class="row">
-									  				<div class="col-sm-1">
-									  					<time datetime="2014-06-29" class="pull-left datebox">
-															<strong>Mar</strong>
-															<span>30</span>
-														</time>
-									  				</div>
-									  				<div class="col-sm-7">
-									  					<h4 title="Your Tutor for this class">
-									  						 Glenn Dumaguing
-									  						<button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#tutorInfo_2">
-									  							<i class="fa fa-user" aria-hidden="true"></i>
-									  						</button>
-									  					</h4>
-									  					<h6>Monday March 30 2021 at 3:30pm</h6>
-									  				</div>
-									  				<div class="col-sm-4 text-right ">
-									  					<div class="row">
-									  						<div class="col-sm-6">
-										  						<a href="#" class="btn btn-danger form-control">CANCEL</a>
-										  					</div>
-										  					<div class="col-sm-6">
-										  						<a href="#" class="btn btn-primary form-control">RESCHED</a>
-															</div>
-									  					</div>
-									  				</div>
-									  			</div>
-									  		</div>
-									  		<div class="card-body">
-									    		ENGLISH FOR BEGGINERS
-									  		</div>
-										</div>
+									<!-- looped Modal -->
+									<div class="modal fade" id="tutorInfo_{{$d}}" tabindex="-1" role="dialog" aria-labelledby="tutorInfo_{{$d}}" aria-hidden="true">
+									  <div class="modal-dialog modal-dialog-centered" role="document">
+									    <div class="modal-content">
+									      <div class="modal-header">
+									        <h5 class="modal-title" id="exampleModalLongTitle">Glenn Dumaguing</h5>
+									        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									          <span aria-hidden="true">&times;</span>
+									        </button>
+									      </div>
+									      <div class="modal-body">
+									      	{{$d}}
+									      </div>
+									      <div class="modal-footer">
+									        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+									        <button type="button" class="btn btn-primary">PROFILE</button>
+									      </div>
+									    </div>
+									  </div>
 									</div>
-								</div>
 
-								<!--end results for groups not joined -->
+									<hr/><!--break to next entry-->
+								@endforeach
+								<!--end results for booked -->
 							</div>
 
 							<div class="col-sm-3">
@@ -151,45 +145,4 @@
 		</div>
 	</div>
 	<!--end myclasses main-->
-</div>
-
-<!-- Modal -->
-<div class="modal fade" id="tutorInfo_1" tabindex="-1" role="dialog" aria-labelledby="tutorInfo_1" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Glenn Dumaguing</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        MODAL ONE
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">PROFILE</button>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- Modal -->
-<div class="modal fade" id="tutorInfo_2" tabindex="-1" role="dialog" aria-labelledby="tutorInfo_2" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Glenn Dumaguing</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      	MODAL TWO
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">PROFILE</button>
-      </div>
-    </div>
-  </div>
 </div>
