cc5265174b848b80d947c23ed0023277cd7aaced
railee091 <74151748+railee091@users.noreply.github.com>
2021-03-31 17:41:34 +0800

    find tutor search options

--- resources/views/student/studentFindTutor.blade.php
+++ resources/views/student/studentFindTutor.blade.php
@@ -31,51 +31,90 @@
 
 						    	<label for="countryCitizenship" class="col-form-label">Country of Citizenship</label>
 						      	<select class="form-control" id="countryCitizenship">
-							      	<option>1</option>
+							      	<option selected>unspecified</option>
 							      	<option>2</option>
 							      	<option>3</option>
-							      	<option>4</option>
-							      	<option>5</option>
 							    </select>
 						    </div>
 						    <div class="col-sm-2">
 						    	<label for="tutorAge" class="col-form-label">Age</label>
-						      	<input type="text" class="form-control" id="tutorAge" placeholder="tutor name e.g JOHN">
+						      	<input type="text" class="form-control" id="tutorAge" placeholder="e.g 30">
 						    </div>
 						    <div class="col-sm-2">
 						    	<label for="tutorAge" class="col-form-label">Gender</label>
 						      	<select class="form-control" id="countryCitizenship">
-							      	<option selected disabled>Select Gender</option>
+							      	<option selected>Unspecified</option>
 							      	<option>Male</option>
 							      	<option>Female</option>
 							    </select>
 						    </div>
 						</div>
-			  			<div class="form-group row">
-						    <div class="col-sm-6">
-						    	<div class="form-check form-check-inline">
-								  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-								  <label class="form-check-label" for="inlineCheckbox1">FREE TALKING</label>
+			  			<div class="form-group">
+			  				<div class="row  d-flex justify-content-center">
+				  				<a class="float-right"data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+							    +Advanced Options
+							  	</a>
+							</div>
+							<br/>
+					    	<div class="collapse row" id="collapseExample">
+					    		<div class="col-sm-12 form-check-inline d-flex justify-content-center">
+						    		<ul class="chec-radio">
+										<li class="pz">
+											<label class="radio-inline">
+												<input type="checkbox" id="pro-chx-residential" name="gas_availability" class="pro-chx" value="yes">
+												<div class="clab">Native Speaker</div>
+											</label>
+										</li>
+										<li class="pz">
+											<label class="radio-inline">
+												<input type="checkbox" id="pro-chx-residential" name="electricity_availability" class="pro-chx" value="yes">
+												<div class="clab">Available for kids</div>
+											</label>
+										</li>
+										<li class="pz">
+											<label class="radio-inline">
+												<input type="checkbox" id="pro-chx-residential" name="phone_internet" class="pro-chx" value="yes">
+												<div class="clab">Free talking</div>
+											</label>
+										</li>
+										<li class="pz">
+											<label class="radio-inline">
+												<input type="checkbox" id="pro-chx-residential" name="water_availability" class="pro-chx" value="yes">
+												<div class="clab">For begginers</div>
+											</label>
+										</li>
+										<li class="pz">
+											<label class="radio-inline">
+												<input type="checkbox" id="pro-chx-residential" name="sanitation" class="pro-chx" value="yes">
+												<div class="clab">Sanitation</div>
+											</label>
+										</li>
+									</ul>
 								</div>
-						    	<div class="form-check form-check-inline">
-								  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-								  <label class="form-check-label" for="inlineCheckbox1">NATIVE SPEAKER</label>
-								</div>
-						    	<div class="form-check form-check-inline">
-								  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-								  <label class="form-check-label" for="inlineCheckbox1">AVALABLE FOR KIDS</label>
-								</div>
-						    	<div class="form-check form-check-inline">
-								  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-								  <label class="form-check-label" for="inlineCheckbox1">FOR BEGGINERS</label>
-								</div>
-						    </div>
+
+					    		<div class="col-sm-4">
+				    				<div class="form-group">
+									    	<label for="tutorName" class="col-form-label">Hobbies & Interest</label>
+									      	<input type="text" class="form-control" id="tutorName" placeholder="e.g Photography">
+									</div>
+					    		</div>
+					    		<div class="col-sm-4">
+				    				<div class="form-group">
+									    	<label for="tutorName" class="col-form-label">Tags</label>
+									      	<input type="text" class="form-control" id="tutorName" placeholder="e.g TOEIC">
+									</div>
+					    		</div>
+					    		<div class="col-sm-4">
+				    				<div class="form-group">
+									    	<label for="tutorName" class="col-form-label">Minimum credits</label>
+									      	<input type="text" class="form-control" id="tutorName" placeholder="e.g 25">
+									</div>
+					    		</div>
+							</div>
 						</div>
 			  		</div>
 				</div>
 			</div>
-			<!-- Middle Column end -->
-		</div>
-	</div>
-	<!--end myclasses main-->
+		</div><!-- Middle Column end -->
+	</div><!--end myclasses main-->
 </div>
