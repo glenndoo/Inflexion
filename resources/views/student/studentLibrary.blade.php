737b172b0a6ea31d69c5a68e810a785f2cfaa43c
railee091 <74151748+railee091@users.noreply.github.com>
2021-05-22 11:41:49 +0800

    basic embed pdf

--- resources/views/student/studentLibrary.blade.php
+++ resources/views/student/studentLibrary.blade.php
@@ -12,7 +12,33 @@
 		<!--start post to feed -->
 		<div class="row">
 			<div class="col-sm-12">
-				<h6>student>studentLibrary</h6>
+				<!-- search bar start-->
+				<div class="group-card card shadow">
+			  		<div class="card-header">
+			  			<div class="row">
+			  				40 books available
+			  			</div>
+			  		</div>
+			  		<div class="card-body">
+			  			<div class="form-group row">
+						    <div class="col-sm-12">
+						    	<!--label for="countryCitizenship" class="col-form-label">Citizenship</label-->
+						      	<select class="form-control" id="countryCitizenship">
+							      	<option selected>unspecified</option>
+							      	<option selected>unspecified</option>
+							      	<option selected>unspecified</option>
+							      	<option selected>unspecified</option>
+							      	<option selected>unspecified</option>
+							      	<option selected>unspecified</option>
+							    </select>
+						    </div>
+						</div>
+			  			<div class="form-group">
+					    	
+						</div>
+			  		</div>
+				</div>
+				<!-- search bar end-->
 			</div>
 		</div>
 		<!--post to feed end-->
@@ -20,6 +46,7 @@
 		<!--settings start-->
 		<div class="row">
 			<div class="col-sm-12">
+				<embed src="https://pdfobject.com/pdf/sample.pdf" type="application/pdf" width="100%" height="700rem">
 			</div>
 		</div>
 		<!--end settings-->
