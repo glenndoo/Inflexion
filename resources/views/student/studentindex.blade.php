4e89131e9b5f447fedc1c52e418fbf8e4ac94d82
railee091 <74151748+railee091@users.noreply.github.com>
2021-05-30 16:10:48 +0800

    Update studentindex.blade.php

--- resources/views/student/studentindex.blade.php
+++ resources/views/student/studentindex.blade.php
@@ -9,75 +9,70 @@
 				<br/>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-sm-12">
-			</div>
-		</div>
 		<!--start post to feed -->
 		<div class="row d-flex justify-content-center">
 			<div class="col-sm-8 ">
 				<form action="{{ route('PostMessage') }}" method="post">
-				@csrf
-				<div class="panel shadow">
-		            <div class="panel-body">
-		        		<textarea class="form-control" rows="2" placeholder="What are you thinking?" name="postMessage" required></textarea><br/>
-		        		<div class="mar-top clearfix">
-		        			<button class="btn btn-sm btn-primary pull-right" type="submit"/><i class="fa fa-pencil-square-o"></i></button>
-							<a class="btn btn-trans btn-icon fa fa-video-camera add-tooltip" href="#" data-original-title="Add Video" data-toggle="tooltip"></a>
-		        			<a class="btn btn-trans btn-icon fa fa-camera add-tooltip" href="#" data-original-title="Add Photo" data-toggle="tooltip"></a>
-		        			<a class="btn btn-trans btn-icon fa fa-file add-tooltip" href="#" data-original-title="Add File" data-toggle="tooltip"></a>
-		        		</div>
-		        	</div>
-		        </div>
+					@csrf
+					<div class="panel shadow">
+	          <div class="panel-body">
+	      			<textarea class="form-control" rows="2" placeholder="What are you thinking?" name="postMessage" required></textarea><br/>
+	      			<div class="mar-top clearfix">
+	      				<button class="btn btn-sm btn-primary pull-right" type="submit"><i class="fa fa-pencil-square-o"></i></button>
+								<a class="btn btn-trans btn-icon fa fa-video-camera add-tooltip" href="#" data-original-title="Add Video" data-toggle="tooltip"></a>
+	      				<a class="btn btn-trans btn-icon fa fa-camera add-tooltip" href="#" data-original-title="Add Photo" data-toggle="tooltip"></a>
+	      				<a class="btn btn-trans btn-icon fa fa-file add-tooltip" href="#" data-original-title="Add File" data-toggle="tooltip"></a>
+	      			</div>
+	      		</div>
+	     		</div>
 				</form>
 			</div>
 		</div>
 		<!--post to feed end-->
 
 		<!--start posts-->
+
+
+		
 		@if(count($details) > 0)
 		@foreach($details as $post)
-		<div class="row d-flex justify-content-center">
+		<div class="row d-flex justify-content-center ">
 			<div class="col-sm-8 ">
 				<div class="social-feed-box shadow">
-			        <div class="pull-right social-action dropdown">
-			            <button class="btn btn-sm"data-toggle="dropdown" class="dropdown-toggle btn-white">
-			                <i class="fa fa-angle-down"></i>
-			            </button>
+	        <div class="pull-right social-action dropdown">
+            <button class="btn btn-sm"data-toggle="dropdown" class="dropdown-toggle btn-white">
+               <i class="fa fa-angle-down"></i>
+            </button>
 						@php
 						$id = $post->inflexion_post_id;
 						@endphp
-			            <ul class="dropdown-menu m-t-xs">
-						@if($post->inflexion_post_poster == session()->get('info.userId'))
-			                <li><a class="postUser" href="#" data-toggle="modal" data-target="#delete-post-{{$id}}">Delete</a></li>
-			                <li><a class="postUser" href="#" data-toggle="modal" data-target="#edit-post-{{$id}}">Edit</a></li>
-			            @else
-			            	<li><a class="postUser" href="" data-toggle="modal" data-target="#report-post-{{$id}}">Report</a></li>
-						@endif
-			            </ul>
-			        </div>
-
-
-			        <div class="social-avatar">
-			            <a href="" class="pull-left">
-			                <img alt="image" src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png">
-			            </a>
-			            <div class="media-body">
-			            	<div class=" form-inline">
-			            		<!--this name of poster-->
-			            		<a href="#">
-				                    {{ $post->inflexion_detail_first.' '.$post->inflexion_detail_last}}
-									@if($post->inflexion_post_poster == session()->get('info.userId'))
-						                (You)
-									@endif
-				                </a>
-				                <!--this trigger for hidden badges of poster note: only if tutor show trigger-->
-				                <button class="btn btn-default btn-sm" type="button" data-toggle="collapse" data-target="#user-badge-{{$id}}" aria-expanded="false" aria-controls="collapseExample">
-									<i class="fa fa-info-circle"></i>
-								</button>
+	          <ul class="dropdown-menu m-t-xs">
+							@if($post->inflexion_post_poster == session()->get('info.userId'))
+		            <li><a class="postUser" href="#" data-toggle="modal" data-target="#delete-post-{{$id}}">Delete</a></li>
+		            <li><a class="postUser" href="#" data-toggle="modal" data-target="#edit-post-{{$id}}">Edit</a></li>
+		          @else
+		            <li><a class="postUser" href="" data-toggle="modal" data-target="#report-post-{{$id}}">Report</a></li>
+							@endif
+	          </ul>
+	        </div>
+        	<div class="social-avatar">
+            <a href="" class="pull-left">
+                <img alt="image" src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png">
+            </a>
+            <div class="media-body">
+            	<div class=" form-inline">
+            		<!--this name of poster-->
+            		<a href="#">
+	              {{ $post->inflexion_detail_first.' '.$post->inflexion_detail_last}}
+								@if($post->inflexion_post_poster == session()->get('info.userId'))
+									(You)
+								@endif
+	              </a>
+	              <!--this trigger for hidden badges of poster note: only if tutor show trigger-->
+	              <button class="btn btn-default btn-sm" type="button" data-toggle="collapse" data-target="#user-badge-{{$id}}" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-info-circle"></i></button>
 								<!--this hidden badges of poster note: only if tutor show badge-->
-				                <div class="collapse" id="user-badge-{{$id}}">
-				                	<span class="badge badge-primary">Primary</span>
+	              <div class="collapse" id="user-badge-{{$id}}">
+	              	<span class="badge badge-primary">Primary</span>
 									<span class="badge badge-secondary">Secondary</span>
 									<span class="badge badge-success">Success</span>
 									<span class="badge badge-danger">Danger</span>
@@ -85,127 +80,103 @@
 									<span class="badge badge-info">Info</span>
 									<span class="badge badge-light">Light</span>
 									<span class="badge badge-dark">Dark</span>
-				                </div>
-			            	</div>
-			                
-			                <small class="text-muted">{{ $post->inflexion_post_timestamp }}</small>
-			            </div>
-			        </div>
-			        <div class="social-body">
-			        	<!--start post body
-						note: (3 part collapse no JS, pure CSS)  put inside collapse_module for collapse property for two line paragraph
-								add html class collapse_long_element for paragraph body
-								add a pr button for collapse toggle (see example below) -Maiko
-			        	-->
+	              </div>
+            	</div>
+              <small class="text-muted">{{ $post->inflexion_post_timestamp }}</small>
+            </div>
+        	</div>
 
-			        	<div class="collapse_module">
-			        		<p class="collapse collapse_long_element" id="collapse_long_element" aria-expanded="false">
-			                	{{ $post->inflexion_post_message }}
-			            	</p>
-							
-			            	@if(strlen($post->inflexion_post_message)>154)
-			            		<a role="button" class="collapsed" data-toggle="collapse" href=".collapse_long_element" aria-expanded="false" aria-controls="collapse_long_element"></a>
-			            	@endif
-			            	
-			        	</div>
-			            
-			            <!--end post body-->
-			            <div class="row post-status">
-			           		<div class="btn-group">
-			            		<div class="col-sm-12">
-			            			<!--@if($post->inflexion_post_like == 1)
-										{{ $post->inflexion_post_like }}<a href="#"><i class="fa fa-thumbs-up">person likes this</i></a>
-									@elseif($post->inflexion_post_like > 1)
-										<a href="">{{ $post->inflexion_post_like }}<i class="fa fa-thumbs-up">people like this</i></a>
-									@else
-									@endif-->
-			            		</div>
-			            	</div>
-			            </div>
-			            <div class="row post-react">
-			            	<div class="btn-group">
-			            		<div class="col-sm-12">
-			            			@if($post->inflexion_post_like == 1)<!--just 1 like)-->
-			            				<a href='{{ route("LikePost", [$id]) }}' title="{{ $post->inflexion_post_like }} user/s has liked this"><i class="fa fa-thumbs-up"></i>{{ $post->inflexion_post_like }} Like</a>
-			            			@elseif($post->inflexion_post_like > 1)<!--more then 1 like)-->
-			            				<a href='{{ route("LikePost", [$id]) }}' title="{{ $post->inflexion_post_like }} user/s has liked this"><i class="fa fa-thumbs-up"></i>{{ $post->inflexion_post_like }} Likes</a>
-			            			@else($post->inflexion_post_like == 0)<!--no likes)-->
-			            				<a href='{{ route("LikePost", [$id]) }}' class="btn btn-white btn-xs"><i class="fa fa-thumbs-up"></i> Like</a>
-			            			@endif
+	        <div class="social-body">
+	        	<div class="collapse_module">
+	        		<p class="collapse collapse_long_element" id="collapse_long_element" aria-expanded="false">{{ $post->inflexion_post_message }}</p>
+	            	@if(strlen($post->inflexion_post_message)>154)
+	            		<a role="button" class="collapsed" data-toggle="collapse" href=".collapse_long_element" aria-expanded="false" aria-controls="collapse_long_element"></a>
+	            	@endif
+	        	</div>
+            <div class="row post-react">
+            	<div class="btn-group">
+            		<div class="col-sm-12">
+            			@if($post->inflexion_post_like == 1)<!--just 1 like)-->
+            				<a href='{{ route("LikePost", [$id]) }}' title="{{ $post->inflexion_post_like }} user/s has liked this"><i class="fa fa-thumbs-up"></i>{{ $post->inflexion_post_like }} Like</a>
+            			@elseif($post->inflexion_post_like > 1)<!--more then 1 like)-->
+            				<a href='{{ route("LikePost", [$id]) }}' title="{{ $post->inflexion_post_like }} user/s has liked this"><i class="fa fa-thumbs-up"></i>{{ $post->inflexion_post_like }} Likes</a>
+            			@else($post->inflexion_post_like == 0)<!--no likes)-->
+            				<a href='{{ route("LikePost", [$id]) }}' class="btn btn-white btn-xs"><i class="fa fa-thumbs-up"></i> Like</a>
+            			@endif
 
-			            			@php
-						        		$rowCount = count($comments->where('poster_id', $id))
-						        	@endphp
-			            			@if($rowCount > 1)<!--more than 1 comment)-->
-					                	<a data-toggle="collapse" href="#social-comment-{{$id}}"><i class="fa fa-comments">{{$rowCount}}</i> Comments</a>
-					                @elseif($rowCount == 1)<!--just 1 comment)-->
-					                	<a data-toggle="collapse" href="#social-comment-{{$id}}"><i class="fa fa-comments">{{$rowCount}}</i> Comment</a>
-					                @else <!--no comments)-->
-					                	<a data-toggle="collapse" href="#social-comment-{{$id}}"><i class="fa fa-comments"></i> Comment</a>
-					                @endif
-					               
-					                <button class="btn btn-white btn-xs"><i class="fa fa-share"></i> Share</button>
+            			@php
+			        		$rowCount = count($comments->where('poster_id', $id))
+			        		@endphp
+            			@if($rowCount > 1)<!--more than 1 comment)-->
+		                	<a data-toggle="collapse" href="#social-comment-{{$id}}"><i class="fa fa-comments">{{$rowCount}}</i> Comments</a>
+		                @elseif($rowCount == 1)<!--just 1 comment)-->
+		                	<a data-toggle="collapse" href="#social-comment-{{$id}}"><i class="fa fa-comments">{{$rowCount}}</i> Comment</a>
+		                @else <!--no comments)-->
+		                	<a data-toggle="collapse" href="#social-comment-{{$id}}"><i class="fa fa-comments"></i> Comment</a>
+		                @endif
+		               
+		                <button class="btn btn-white btn-xs"><i class="fa fa-share"></i> Share</button>
 								</div>
-			            	</div>
-			            </div>
-			        </div>
+            	</div>
+            </div>
+	        </div>
 
-				        <div class="social-footer collapse" id="social-comment-{{$id}}">
+		      <div class="social-footer collapse" id="social-comment-{{$id}}">
 						@foreach($comments as $comment)
 						@if($id == $comment->poster_id )
-			        	<!-- this comment-->
-			            <div class="social-comment">
-			                <div class="pull-right social-action dropdown">
-					            @if($comment->comment_id == session()->get('info.userId'))
-					            <button class="btn btn-sm"data-toggle="dropdown" class="dropdown-toggle btn-white">
-					                <i class="fa fa-angle-down"></i>
-					            </button>
-					            <ul class="dropdown-menu m-t-xs">
-					                <li><a class="postUser" href="">Delete</a></li>
-					                <li><a class="postUser" href="#">Edit</a></li>
-					            </ul>
-					            @endif
-					        </div>
-			                <a href="" class="pull-left">
-			                    <img alt="image" src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png">
-			                </a>
-			                <div class="media-body">
-			                    <a href="#">
-			                        
-			                    {{ $comment->inflexion_detail_first.' '.$comment->inflexion_detail_last  }}
-			                    </a>
-			                    {{ $comment->comment_message }}
-			                    <br>
-			                    <a href="#" class="small"><i class="fa fa-thumbs-up"></i> 26 Like this!</a> -
-			                    <small class="text-muted">12.06.2014</small>
-								
-			                </div>
-			            </div>
-			            @endif
-			            @endforeach
+	        	<!-- this comment-->
 
-			            <!-- this write comment part-->
-			            <div class="row social-comment">
-			            	<div class="col-sm-1">
-			            		<a href="" class="pull-left">
-			                    <img alt="image" src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png">
-				                </a>
-			            	</div>
-			            	<div class="col-sm-11">
-				                <div class="media-body">
-									<form method="POST" action="{{ route('CommentPost', [$id]) }}">
-										@csrf
-										<input type='hidden' value='{{ $id }}' name='postId'/>
-					                    <input type='textarea' class="form-control" placeholder="Write comment..." name='commentMessage' required/>
-										<input type='submit' class="float-right btn btn-sm btn-primary"value='Post Comment' />
-									</form>
-				                </div>
+	        	<div class="row">
+	        		<div class="col-sm-12">
+	        			<div class="social-comment">
+			          	@if($comment->comment_id == session()->get('info.userId'))
+			            <div class="pull-right social-action dropdown">
+				            <button class="btn btn-sm"data-toggle="dropdown" class="dropdown-toggle btn-white"><i class="fa fa-angle-down"></i></button>
+				            <ul class="dropdown-menu m-t-xs">
+				              <li><a class="postUser" href="">Delete</a></li>
+				              <li><a class="postUser" href="#">Edit</a></li>
+				            </ul>
+				        	</div>
+				          @endif
+			            <a href="" class="pull-left"><img alt="image" src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"></a>
+			            <div class="media-body">
+		               
+		               	 <b><a href="#">{{ $comment->inflexion_detail_first.' '.$comment->inflexion_detail_last  }}</a></b>
+		                {{ $comment->comment_message }}
+		                <br>
+		                <a href="#" class="small"><i class="fa fa-thumbs-up"></i> 26 Like this!</a> -
+		                <small class="text-muted">12.06.2014</small>
+			            </div>
+			          </div>
+	        		</div>
+	        	</div>
+	          
+	          @endif
+	          @endforeach
+	          <div class="social-comment">
+		          <div class="row">
+								<form class="col-sm-12" method="POST" action="{{ route('CommentPost', [$id]) }}">
+									@csrf
+		        			<div class="row">
+		        				<div class="col-sm-1">
+				          		<a href="" class="pull-left"><img alt="image" src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"></a>
+				          	</div>
+				          	<div class="col-sm-8">
+				              <div class="media-body">
+												<input type='hidden' value='{{ $id }}' name='postId'/>
+					              <input type='textarea' class="form-control" placeholder="Write comment..." name='commentMessage' required/>
+				              </div>
 				            </div>
-			            </div>
 
-			        </div>
-
-			    </div>
+				            <div class="col-sm-3">
+				            	<input type='submit' class="form-control btn btn-sm btn-primary"value='Comment' />
+				            </div>
+		        			</div>
+					      </form>
+		        	</div>
+		        </div>
+	        </div>
+			  </div>
 			</div>
 		</div>
 		@endforeach
